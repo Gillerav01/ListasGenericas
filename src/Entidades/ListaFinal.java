8f21b9f9462c38943406125f9845a84a20a35755
Guillermo <gillerav01@educantabria.es>
2021-09-28 23:26:40 +0200

    Funcional

--- src/Entidades/ListaFinal.java
+++ src/Entidades/ListaFinal.java
@@ -1,20 +1,20 @@
 package Entidades;
 
-public class ListaFinal {
+public class ListaFinal <T> {
 
-    private Nodo prim;
-    private Nodo fin;
+    private Nodo<T> prim;
+    private Nodo<T> fin;
 
     public ListaFinal() {
         this.prim = null;
         this.fin = null;
     }
 
-    public Nodo getPrim() {
+    public Nodo<T> getPrim() {
         return prim;
     }
 
-    public void setPrim(Nodo prim) {
+    public void setPrim(Nodo<T> prim) {
         this.prim = prim;
     }
 
@@ -22,7 +22,7 @@ public class ListaFinal {
         return fin;
     }
 
-    public void setFin(Nodo fin) {
+    public void setFin(Nodo<T> fin) {
         this.fin = fin;
     }
 
@@ -30,39 +30,101 @@ public class ListaFinal {
         return this.prim == null && this.fin == null;
     }
 
-    public void add(int x) {
-        
+    public void add(T x) {
+        Nodo<T> nuevo = new Nodo(x, null);
+
+        if (this.esVacia()) {
+            prim = nuevo;
+            fin = nuevo;
+        } else {
+            nuevo.setSig(prim);
+            prim = nuevo;
+        }
     }
 
-    public void addFin(int x) {
-        
+    public void addFin(T x) {
+        Nodo<T> nuevo = new Nodo(x, null);
+        if (this.esVacia()) {
+            prim = nuevo;
+            fin = nuevo;
+        } else {
+            fin.setSig(nuevo);
+            fin = nuevo;
+        }
     }
 
     public void leer() {
-        
+        Nodo<T> aux = prim;
+        System.out.println("\n" + "--------------------------");
+        while (aux != null) {
+            System.out.print(aux.getInfo() + " ");
+            aux = aux.getSig();
+        }
     }
 
-    public boolean borrar(int x) {
-        
+    public boolean borrar(T x) {
+        boolean encontrado = false;
+        Nodo<T> aux = prim;
+        Nodo<T> ant = null;
+
+        while (aux != null) {
+            if (aux.getInfo() == x) {
+                if (ant == null) {
+                    this.prim = aux.getSig();
+                } else {
+                    ant.setSig(aux.getSig());
+                }
+                encontrado = true;
+            } else {
+                ant = aux;
+            }
+            aux = aux.getSig();
+        }
+
+        return encontrado;
     }
 
     public void borrarPrimero() {
-        
+        if (!this.esVacia()) {
+            this.prim = prim.getSig();
+        }
     }
 
-    public int leerPrimero() {
-        return 1;
+    public T leerPrimero() {
+        T x = null;
+        if (!esVacia()) {
+            x = prim.getInfo();
+        }
+        return x;
     }
     
     public void concatenar(ListaFinal lf) {
-
+        Nodo<T> aux = lf.prim;
+        while (aux != null) {
+            this.addFin(aux.getInfo());
+            aux = aux.getSig();
+        }
     }
     
     public int length () {
-
+        int ret = 0;
+        Nodo aux = this.prim;
+        
+        while (aux != null) {
+            ret++;
+            aux = aux.getSig();
+        }
+        return ret;
     }
-
+    
     public void reverse () {
-
+        ListaFinal l = this;
+        Nodo<T> aux = l.getPrim();
+        this.prim = null;
+        this.fin = null;
+        while (aux != null) {
+           this.add(aux.getInfo());
+           aux = aux.getSig();
         }
     }
+}
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,7 +1,23 @@
 package Main;
+
+import Entidades.ListaFinal;
+import Entidades.Persona;
+
 public class Main {
 
     public static void main(String[] args) {
+        Persona Guillermo = new Persona(23, "Guillermo ", "Illera ", "21141255T. " );
+        Persona Alex = new Persona(18, "Alex ", "Ramales  ", "21141543T. " );
+        Persona Nina = new Persona(19, "Nina ", "Cojones ", "21141123H. " );
+        
+        ListaFinal lf = new ListaFinal();
+        lf.add(Guillermo);
+        lf.add(Alex);
+        lf.add(Nina);
+        
+        lf.leer();
+        
+        
         
     }
     
